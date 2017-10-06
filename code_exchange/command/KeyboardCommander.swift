74987ed3682f7b5b0ef631d3607afe7c53c6af95
FENG <rfeng@doh.health.nsw.gov.au>
2017-10-06 17:02:08 +1100

    git2-design in commander

--- code_exchange/command/KeyboardCommander.swift
+++ code_exchange/command/KeyboardCommander.swift
@@ -5,5 +5,30 @@
 //  Created by Wanlou Feng on 1/10/17.
 //  Copyright © 2017 Wanlou Feng. All rights reserved.
 //
+/*
+Start-up
+KeyView Touches Down
+KeyView Click
+KeyView Double Click
+KeyView Long Press
+Close-up 
 
+KeyboardViewController
+KeyboardView
+
+KeyboardDefintion
+
+
+UpperLetterPage
+LowerLetterPage
+SymbolPage
+NumberPage
+
+Most-Used-Hanzi-Page
+ShengMuPage
+PinyinPerShengMuPage 
+HanziPhrasePage generated from dictionary
+
+
+*/
 import Foundation
