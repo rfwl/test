a271a6eb01e57b21cf4ded816b532a4063571753
FENG <rfeng@doh.health.nsw.gov.au>
2017-10-09 14:27:03 +1100

    git2-main and 2nd cell frames

--- prod/zdios2/zdios202/model/KeyboardKey.swift
+++ prod/zdios2/zdios202/model/KeyboardKey.swift
@@ -32,6 +32,9 @@ class KeyboardKey {
     func getMainCell() -> KeyCell {
         return keyCellArray[0]
     }
+    
+    
+   
     //================================================
    	// Inits
    	init(_ char:String) {
@@ -68,9 +71,77 @@ class KeyboardKey {
     
     //================================================
    	// Transient Properties
+    var keyView:KeyView?
     
-   	var view:UIView?
+    //================================================
+   	// Cells 
+    var mainCellFrame:CGRect = CGRect.zero
+    var secondaryCellFrame:CGRect = CGRect.zero
+   	func setMainAndSecondaryFrames(){
+   		switch self.cellConfiguration {
+   		case .SingleCell:
+   		case .SingleCellPlusPopUpCells:
+   			mainCellFrame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+   			secondaryCellFrame = CGRect.zero
+   		break
+   		case .SecondaryCell:
+   		case .SecondarCellPlusPopUpCells:
+   			calculateMainCellFrame()
+  			calculateSecondaryCellFrame()		
+   		break
+   		default:
+   			mainCellFrame = CGRect.zero
+    		secondaryCellFrame = CGRect.zero
+   		break
+   		}
+    } //end of func
+   	
+   	 func calculateSecondaryCellFrame(){
+    	let w = self.frame.width * self.secondaryCellSizeScale
+    	let h = self.frame.height * self.secondaryCellSizeScale
+    	switch self.secodaryCellLocation {
+    	case TopLeft:
+    		self.secondaryCellFrame = CGRect(x: 0, y: 0, width: w, height: h)
+    		break
+    	case TopRight:
+    		self.secondaryCellFrame = CGRect(x: self.frame.width-w, y: 0, width: w, height: h)
+    		break
+    	case BottomLeft:
+    		self.secondaryCellFrame = CGRect(x: 0, y: self.frame.height-h, width: w, height: h)
+    		break
+        case BottomRight:    
+    		self.secondaryCellFrame = CGRect(x: self.frame.width, y: self.frame.height-h, width: w, height: h)
+    		break
+        default:
+        	self.secondaryCellFrame = CGRect.zero
+        break
+        }
+    }
     
+    func calculateMainCellFrame(){
+    	let w = self.frame.width 
+    	let sh = self.frame.height * self.mainCellSizeScale
+    	let h = self.frame.height - sh 
+    	switch self.secodaryCellLocation {
+    	case TopLeft:
+    	case TopRight:
+    		self.mainCellFrame = CGRect(x: 0, y: sh, width: w, height: h )
+ 			break
+    	case BottomLeft:
+        case BottomRight:    
+    		self.mainCellFrame = CGRect(x: 0, y: 0, width: w, height: h)
+    		break
+        default:
+        	self.mainCellFrame = CGRect.zero
+        break
+        }
+    }
+   	//================================================
+   	// Pop-up
+   	
+   	
+   	
+   	    
     //================================================
    	//
 } // end of class
