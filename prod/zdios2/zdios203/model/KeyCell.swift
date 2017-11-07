cc842dfc271b978b2f1dde56a78e37fbd1c345cf
FENG <rfeng@doh.health.nsw.gov.au>
2017-11-07 17:12:55 +1100

    git2-design use cases

--- prod/zdios2/zdios203/model/KeyCell.swift
+++ prod/zdios2/zdios203/model/KeyCell.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 class KeyCell {
-    var output:String
+    var name:String
     var labelingType:EnumLabelingType
     var widthInPopUpUnit:CGFloat = CGFloat(1)
     
