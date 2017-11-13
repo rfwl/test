9abc31dc5b33aa26ea5810ffd94455cef270f9ca
FENG <rfeng@doh.health.nsw.gov.au>
2017-11-13 17:09:25 +1100

    git2-build json string from swift codes

---
+++ prod/zdios2/zdios203/model/KeyboardDefintionJsonBuilder.swift
@@ -0,0 +1,193 @@
+//============================================
+let kbd = """
+{
+	"name" : "DefaultKeyboardDefinition",
+	"text" : "English Keyboard",
+	"pages" : 
+	[ 
+"""
+let array_object_close = """
+	]
+}
+"""
+//============================================
+let p1 = """
+ 		{
+ 			"name" : "P1",
+ 			"text" : "Page Uppercase",
+ 			"rows" : [
+"""
+let p2 = """
+ 		{
+ 			"name" : "P2",
+ 			"text" : "Page Lowercase",
+ 			"rows" : [
+"""
+let p3 = """
+ 		{
+ 			"name" : "P3",
+ 			"text" : "Page Number",
+ 			"rows" : [
+"""
+let p4 = """
+ 		{
+ 			"name" : "P4",
+ 			"text" : "Page Symbol",
+ 			"rows" : [
+"""
+//============================================
+let p1r1 = """
+ 		{
+ 			"name" : "P1R1",
+ 			"text" : "Page 1 Row 1",
+ 			"keys" : [
+"""
+let p1r2 = """
+ 		{
+ 			"name" : "P1R2",
+ 			"text" : "Page 1 Row 2",
+ 			"keys" : [
+"""
+let p1r3 = """
+ 		{
+ 			"name" : "P1R3",
+ 			"text" : "Page 1 Row 3",
+ 			"keys" : [
+"""
+let p1r4 = """
+ 		{
+ 			"name" : "P1R4",
+ 			"text" : "Page 1 Row 4",
+ 			"keys" : [
+"""
+//============================================
+
+let p2r1 = """
+ 		{
+ 			"name" : "P2R1",
+ 			"text" : "Page 2 Row 1",
+ 			"keys" : [
+"""
+let p2r2 = """
+ 		{
+ 			"name" : "P2R2",
+ 			"text" : "Page 2 Row 2",
+ 			"keys" : [
+"""
+let p2r3 = """
+ 		{
+ 			"name" : "P2R3",
+ 			"text" : "Page 2 Row 3",
+ 			"keys" : [
+"""
+let p2r4 = """
+ 		{
+ 			"name" : "P2R4",
+ 			"text" : "Page 2 Row 4",
+ 			"keys" : [
+"""
+//============================================
+let p3r1 = """
+ 		{
+ 			"name" : "P3R1",
+ 			"text" : "Page 3 Row 1",
+ 			"keys" : [
+"""
+let p3r2 = """
+ 		{
+ 			"name" : "P3R2",
+ 			"text" : "Page 3 Row 2",
+ 			"keys" : [
+"""
+let p3r3 = """
+ 		{
+ 			"name" : "P3R3",
+ 			"text" : "Page 3 Row 3",
+ 			"keys" : [
+"""
+let p3r4 = """
+ 		{
+ 			"name" : "P3R4",
+ 			"text" : "Page 3 Row 4",
+ 			"keys" : [
+"""
+//============================================
+let p1r1 = """
+ 		{
+ 			"name" : "P1R1",
+ 			"text" : "Page 1 Row 1",
+ 			"keys" : [
+"""
+let p1r2 = """
+ 		{
+ 			"name" : "P1R2",
+ 			"text" : "Page 1 Row 2",
+ 			"keys" : [
+"""
+let p1r3 = """
+ 		{
+ 			"name" : "P1R3",
+ 			"text" : "Page 1 Row 3",
+ 			"keys" : [
+"""
+let p1r4 = """
+ 		{
+ 			"name" : "P1R4",
+ 			"text" : "Page 1 Row 4",
+ 			"keys" : [
+"""
+//============================================
+
+
+
+
+
+
+let 
+{
+	"name" : "DefaultKeyboardDefinition",
+	"text" : "Minimal English Keyboard",
+	"pages" : 
+	[
+
+ 		{
+ 			"name" : "P1",
+ 			"text" : "Page Uppercase",
+ 			"rows" : [
+   				"name" : "P1R1",
+   				"text" : "Row 1",
+   				"keys" : [
+
+   							{
+								"name" : "A", 
+								"text" : "A", 
+								"mainCellArray" : [
+ca,cb,cc
+], 
+"secondaryCellArray" : [
+
+cd,ce,cf], 
+
+"popUpCellArray" : [cg,ch,ci,cj], widthInRowUnits : 1}
+
+{ {"name":"P1J","text":"J"}
+
+},
+
+
+]
+
+},
+{
+
+},
+
+{ 
+
+
+}
+
+]
+
+}
+
